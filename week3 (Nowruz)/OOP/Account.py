050f710b76e5207d55ab23321dbaedad13b6e73d
max <max.shiri.90@gmail.com>
2022-04-04 12:17:08 +0430

    nowruz

--- week3 (Nowruz)/OOP/Account.py
+++ week3 (Nowruz)/OOP/Account.py
@@ -8,26 +8,26 @@ class Account:
         if re.match('^[a-zA-Z]+_[a-zA-Z]+$', username):
             self.username = username
         else:
-            raise Exception("invalid username")
+            raise Exception('invalid username')
 
         if re.match('^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)){8,40}', password):
             self.password = hashlib.sha256(password.encode('utf-8')).hexdigest()
         else:
-            raise Exception("invalid password")
+            raise Exception('invalid password')
 
         if re.match('^09\d{9}$', phone_number):
             self.phone_number = phone_number
         elif re.match('^\+989\d{9}$', phone_number):
             self.phone_number = phone_number
         else:
-            raise Exception("invalid phone number")
+            raise Exception('invalid phone number')
 
         if re.match('^([\w\.\-]+)@([\w\.\-]+)((\.([a-zA-Z]){2,5}))$', email):
             self.email = email
         else:
-            raise Exception("invalid email")
+            raise Exception('invalid email')
 
 
-acc1 = Account('Masoud_Shiri', 'Aa123123', '09118873087', 'max.shiri.90@gmail.com')
+user_1 = Account('Masoud_Shiri', 'Aa123123', '09118873087', 'max.shiri.90@gmail.com')
 
-print(acc1.username)
+print(user_1.username)
--- week3 (Nowruz)/OOP/Site.py
+++ week3 (Nowruz)/OOP/Site.py
@@ -1,3 +1,6 @@
+import hashlib
+
+
 class Site:
     def __init__(self, url, register_users, active_users):
         self.url = url
@@ -6,18 +9,48 @@ class Site:
 
     def register(self, user):
         username = user.username
-        if username in self.register_users:
-            raise Exception("user already registered")
+        if username in self.register_users[0]:
+            raise Exception('user already registered')
         else:
             self.register_users.append([username, user.password, user.phone_number, user.email])
-            return "register successful"
+            return 'register successful'
 
-    def login(self):
-        pass
+    def login(self, user):
+        password = hashlib.sha256(user['password'].encode('utf-8')).hexdigest()
+
+        if 'username' and 'password' and 'email' in user:
+            if password == self.register_users[1] and user['email'] == self.register_users[-1]:
+                self.active_users.append(self.register_users[0])
+                return 'login successful'
+            elif self.register_users[0] in self.active_users:
+                return 'user already logged in'
+            else:
+                return 'invalid login'
+
+        elif 'username' and 'password' in user:
+            if password == self.register_users[1]:
+                self.active_users.append(self.register_users[0])
+                return 'login successful'
+            elif self.register_users[0] in self.active_users:
+                return 'user already logged in'
+            else:
+                return 'invalid login'
+
+        elif 'password' and 'email' in user:
+            if password == self.register_users[1]:
+                self.active_users.append(self.register_users[0])
+                return 'login successful'
+            elif self.register_users[0] in self.active_users:
+                return 'user already logged in'
+            else:
+                return 'invalid login'
+
+        else:
+            return 'invalid login'
 
     def logout(self, user):
         if user.username in self.active_users:
             self.active_users.remove(user.username)
-            return "logout successful"
+            return 'logout successful'
         else:
-            return "user is not logged in"
+            return 'user is not logged in'
