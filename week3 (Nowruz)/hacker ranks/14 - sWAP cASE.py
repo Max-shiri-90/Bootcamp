1ca0cfd1b1ca836f1d5b6f119101c57437d4d463
max <max.shiri.90@gmail.com>
2022-04-04 20:07:26 +0430

    hackerranks

--- week3 (Nowruz)/hacker ranks/14 - sWAP cASE.py
+++ week3 (Nowruz)/hacker ranks/14 - sWAP cASE.py
@@ -1,13 +1,13 @@
 def swap_case(s):
     num = ""
-    for let in s:
-        if let.isupper() == True:
-            num += (let.lower())
+    for elm in s:
+        if elm.isupper() == True:
+            num += (elm.lower())
         else:
-            num += (let.upper())
+            num += (elm.upper())
     return num
-
-
+    
+    
 if __name__ == '__main__':
     s = input()
     result = swap_case(s)
--- week3 (Nowruz)/hacker ranks/18 - Find a string.py
+++ week3 (Nowruz)/hacker ranks/18 - Find a string.py
@@ -1,9 +1,9 @@
 def count_substring(string, sub_string):
     count = 0
-    for i in range(0, len(string) - len(sub_string) + 1):
+    for i in range(len(string) - len(sub_string) + 1):
         if string[i] == sub_string[0]:
             flag = 1
-            for j in range(0, len(sub_string)):
+            for j in range(len(sub_string)):
                 if string[i + j] != sub_string[j]:
                     flag = 0
                     break
