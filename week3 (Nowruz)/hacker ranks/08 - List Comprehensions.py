d1a3b8e1d47a568c1475f81eb0df547e4049c48a
max <max.shiri.90@gmail.com>
2022-04-09 11:10:54 +0430

    hacker ranks

--- week3 (Nowruz)/hacker ranks/08 - List Comprehensions.py
+++ week3 (Nowruz)/hacker ranks/08 - List Comprehensions.py
@@ -3,12 +3,10 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
-    final_lst = []
-    inner_lst = []
-    for x in range(x + 1):
-        for y in range(y + 1):
-            for z in range(z + 1):
-                if x + y + z != n:
-                    inner_lst = [x, y, z]
-                    final_lst.append(inner_lst)
-print(final_lst)
+all_list = []
+for x in range(x + 1):
+    for y in range(y + 1):
+        for z in range(z + 1):
+            all_list.append([x, y, z])
+lst = [elm for elm in all_list if sum(elm) != n]
+print(lst)
--- week3 (Nowruz)/hacker ranks/23 - String Formatting.py
+++ week3 (Nowruz)/hacker ranks/23 - String Formatting.py
@@ -1,18 +1,14 @@
 def print_formatted(number):
-    
     width = len(bin(number)[2:])
-    for i in range(1, number+1):
-        deci = str(i)
-        octa = oct(i)[2:]
-        hexa = hex(i)[2:].upper()
-        bina = bin(i)[2:]
-        
-        print(deci.rjust(width),
-        octa.rjust(width),
-        hexa.rjust(width),
-        bina.rjust(width))
-        
-        
+    for i in range(1, number + 1):
+        print(
+            str(i).rjust(width),
+            oct(i)[2:].rjust(width),
+            hex(i)[2:].rjust(width).upper(),
+            bin(i)[2:].rjust(width)
+        )
+
+
 if __name__ == '__main__':
     n = int(input())
     print_formatted(n)
